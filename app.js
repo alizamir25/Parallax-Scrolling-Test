ac9bceba437dd1dc1ae750307ce8e8332343d7d9
alizamir25 <ali0786zamir@gmail.com>
2024-08-26 12:19:31 +0100

    Patch

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-let completed = false;
-function toggleCompleted(value) {
-    completed = value;
+let completed=false;
+function toggleCompleted(value){
+    completed=value;
 }
-function goToSection(i) {
-    gsap.to(window, {
-        scrollTo: { y: i * window.innerHeight, autoKill: false },
-        duration: 1,
-        overwrite: true,
+function goToSection(i){
+    gsap.to(window,{
+        scrollTo:{y:i * window.innerHeight,autoKill:false},
+        duration:1,
+        overwrite: rue,
     });
 }
-document.addEventListener('DOMContentLoaded', function () {
-    const panels = document.querySelectorAll('.panel');
-    panels.forEach((panel, i) => {
+document.addEventListener('DOMContentLoaded',function(){
+    const panels=document.querySelectorAll('.panel');
+    panels.forEach((panel, i)=>{
         ScrollTrigger.create({
-            trigger: panel,
-            start: 'top bottom',
-            end: '+=200%',
-            onToggle: (self) => self.isActive && completed && goToSection(i),
+            trigger:panel,
+            start:'top bottom',
+            end:'+=200%',
+            onToggle:(self)=>self.isActive&&completed&&goToSection(i),
         });
     });
     ScrollTrigger.create({
-        start: 0,
-        end: 'max',
-        snap: 1 / (panels.length - 1),
+        start:0,
+        end:'max',
+        snap:1/(panels.length-1),
     });
     // Toggle completed state after some condition, e.g., after a page load
     toggleCompleted(true);
